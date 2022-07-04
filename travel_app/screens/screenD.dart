33c44e5cd57fb3b3d314881248f9a0d6b91b0efa
Baasith Fazil <67113888+Baasith231@users.noreply.github.com>
2022-07-04 12:55:12 +0530

    Add files via upload

---
+++ travel_app/screens/screenD.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+
+class screenD extends StatelessWidget {
+  const screenD({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("D", style: TextStyle(fontSize:30),),
+    );
+  }
+}
